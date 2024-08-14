9ba7d38fde7395f8734fa744bd38be650def4fc5
canifty <can-dindar@outlook.com>
2024-08-14 11:02:33 -0400

    Initial Commit

---
+++ ListPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ListPractice
+//
+//  Created by Can Dindar on 2024-08-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ListPractice/ListPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListPracticeApp.swift
+//  ListPractice
+//
+//  Created by Can Dindar on 2024-08-14.
+//
+
+import SwiftUI
+
+@main
+struct ListPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
